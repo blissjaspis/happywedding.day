2284eb576cd48401e73af505837b429fca2a5132
Bliss Jaspis <jaspis_zzt@ymail.com>
2024-08-08 11:25:22 +0700

    update

--- components/EventLocation.tsx
+++ components/EventLocation.tsx
@@ -16,135 +16,139 @@ const EventLocation = () => {
                 Lokasi Acara
             </h2>
 
-            <div className="bg-gray-50 p-5">
-                <div className="bg-white p-5 rounded-lg">
-                    <h4 className="uppercase font-semibold text-cyan-800">
-                        Pemberkatan
-                    </h4>
-                    <hr className="w-10 border-t-2 border-cyan-900 opacity-70 mb-5" />
+            <div className="grid grid-cols-1 md:grid-cols-2">
 
-                    <p className="text-sm text-gray-600 mb-5">
-                        Gereja tempat upacara pernikahan akan berlangsung pada
-                        tanggal 20 september di GMAHK Binjai
-                    </p>
+                <div className="bg-gray-50 p-5">
+                    <div className="bg-white p-5 rounded-lg">
+                        <h4 className="uppercase font-semibold text-cyan-800">
+                            Pemberkatan
+                        </h4>
+                        <hr className="w-10 border-t-2 border-cyan-900 opacity-70 mb-5" />
 
-                    <div className="flex items-center mb-5">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6 mr-5 text-cyan-800"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
-                            />
-                        </svg>
+                        <p className="text-sm text-gray-600 mb-5">
+                            Gereja tempat upacara pernikahan akan berlangsung pada
+                            tanggal 20 september di GMAHK Binjai
+                        </p>
 
-                        <div className="flex flex-col text-cyan-800 text-sm">
-                            <span>Selasa, 4 Oktober 2023</span>
-                            <span>Pukul 09:00 - 12:00 WIB</span>
+                        <div className="flex items-center mb-5">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6 mr-5 text-cyan-800"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+                                />
+                            </svg>
+
+                            <div className="flex flex-col text-cyan-800 text-sm">
+                                <span>Selasa, 4 Oktober 2023</span>
+                                <span>Pukul 09:00 - 12:00 WIB</span>
+                            </div>
+                        </div>
+
+                        <div className="flex items-center mb-5">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6 mr-5 text-cyan-800"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M12 8.25v-1.5m0 1.5c-1.355 0-2.697.056-4.024.166C6.845 8.51 6 9.473 6 10.608v2.513m6-4.87c1.355 0 2.697.055 4.024.165C17.155 8.51 18 9.473 18 10.608v2.513m-3-4.87v-1.5m-6 1.5v-1.5m12 9.75l-1.5.75a3.354 3.354 0 01-3 0 3.354 3.354 0 00-3 0 3.354 3.354 0 01-3 0 3.354 3.354 0 00-3 0 3.354 3.354 0 01-3 0L3 16.5m15-3.38a48.474 48.474 0 00-6-.37c-2.032 0-4.034.125-6 .37m12 0c.39.049.777.102 1.163.16 1.07.16 1.837 1.094 1.837 2.175v5.17c0 .62-.504 1.124-1.125 1.124H4.125A1.125 1.125 0 013 20.625v-5.17c0-1.08.768-2.014 1.837-2.174A47.78 47.78 0 016 13.12M12.265 3.11a.375.375 0 11-.53 0L12 2.845l.265.265zm-3 0a.375.375 0 11-.53 0L9 2.845l.265.265zm6 0a.375.375 0 11-.53 0L15 2.845l.265.265z"
+                                />
+                            </svg>
+
+                            <div className="flex flex-col text-cyan-800 text-sm">
+                                <span>GMAHK, Binjai, Sumatera Utara</span>
+                            </div>
+                        </div>
+
+                        <div className="flex justify-center">
+                            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                    Lihat Maps
+                                </span>
+                            </button>
                         </div>
                     </div>
+                </div>
 
-                    <div className="flex items-center mb-5">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6 mr-5 text-cyan-800"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M12 8.25v-1.5m0 1.5c-1.355 0-2.697.056-4.024.166C6.845 8.51 6 9.473 6 10.608v2.513m6-4.87c1.355 0 2.697.055 4.024.165C17.155 8.51 18 9.473 18 10.608v2.513m-3-4.87v-1.5m-6 1.5v-1.5m12 9.75l-1.5.75a3.354 3.354 0 01-3 0 3.354 3.354 0 00-3 0 3.354 3.354 0 01-3 0 3.354 3.354 0 00-3 0 3.354 3.354 0 01-3 0L3 16.5m15-3.38a48.474 48.474 0 00-6-.37c-2.032 0-4.034.125-6 .37m12 0c.39.049.777.102 1.163.16 1.07.16 1.837 1.094 1.837 2.175v5.17c0 .62-.504 1.124-1.125 1.124H4.125A1.125 1.125 0 013 20.625v-5.17c0-1.08.768-2.014 1.837-2.174A47.78 47.78 0 016 13.12M12.265 3.11a.375.375 0 11-.53 0L12 2.845l.265.265zm-3 0a.375.375 0 11-.53 0L9 2.845l.265.265zm6 0a.375.375 0 11-.53 0L15 2.845l.265.265z"
-                            />
-                        </svg>
+                <div className="bg-gray-50 p-5">
+                    <div className="bg-white p-5 rounded-lg">
+                        <h4 className="uppercase font-semibold text-cyan-800">
+                            Resepsi & Adat
+                        </h4>
+                        <hr className="w-10 border-t-2 border-cyan-900 opacity-70 mb-5" />
 
-                        <div className="flex flex-col text-cyan-800 text-sm">
-                            <span>GMAHK, Binjai, Sumatera Utara</span>
+                        <p className="text-sm text-gray-600 mb-5">
+                            Resepsi dan Adat pernikahan akan berlangsung di Tomuria
+                            Hall Binjai
+                        </p>
+
+                        <div className="flex items-center mb-5">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6 mr-5 text-cyan-800"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+                                />
+                            </svg>
+
+                            <div className="flex flex-col text-cyan-800 text-sm">
+                                <span>Selasa, 4 Oktober 2023</span>
+                                <span>Pukul 09:00 - 12:00 WIB</span>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="flex justify-center">
-                        <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                                Lihat Maps
-                            </span>
-                        </button>
+                        <div className="flex items-center mb-5">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6 mr-5 text-cyan-800"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21"
+                                />
+                            </svg>
+
+                            <div className="flex flex-col text-cyan-800 text-sm">
+                                <span>Tomuria Hall, Binjai, Sumatera Utara</span>
+                            </div>
+                        </div>
+
+                        <div className="flex justify-center">
+                            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                    Lihat Maps
+                                </span>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div className="bg-gray-50 p-5">
-                <div className="bg-white p-5 rounded-lg">
-                    <h4 className="uppercase font-semibold text-cyan-800">
-                        Resepsi & Adat
-                    </h4>
-                    <hr className="w-10 border-t-2 border-cyan-900 opacity-70 mb-5" />
-
-                    <p className="text-sm text-gray-600 mb-5">
-                        Resepsi dan Adat pernikahan akan berlangsung di Tomuria
-                        Hall Binjai
-                    </p>
-
-                    <div className="flex items-center mb-5">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6 mr-5 text-cyan-800"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
-                            />
-                        </svg>
-
-                        <div className="flex flex-col text-cyan-800 text-sm">
-                            <span>Selasa, 4 Oktober 2023</span>
-                            <span>Pukul 09:00 - 12:00 WIB</span>
-                        </div>
-                    </div>
-
-                    <div className="flex items-center mb-5">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6 mr-5 text-cyan-800"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21"
-                            />
-                        </svg>
-
-                        <div className="flex flex-col text-cyan-800 text-sm">
-                            <span>Tomuria Hall, Binjai, Sumatera Utara</span>
-                        </div>
-                    </div>
-
-                    <div className="flex justify-center">
-                        <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                                Lihat Maps
-                            </span>
-                        </button>
-                    </div>
-                </div>
-            </div>
 
             <div className="flex justify-center py-10">
                 <button className="flex items-center bg-black text-white px-5 py-3 rounded hover:bg-gray-900 text-sm font-semibold">
